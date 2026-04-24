81a747c5457cb4d6494d42594cedf152b7caff8f
Sheikh K M M Tahmid <sheikh.k.m.m.tahmid@gmail.com>
2026-04-24 20:40:47 +0100

    Add PostgreSQL support via env var; keep MySQL as default for local dev

--- trialguard/settings.py
+++ trialguard/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-trialguard-change-in-production-9x7k2p')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,18 +54,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'trialguard.wsgi.application'
 ASGI_APPLICATION = 'trialguard.asgi.application'
 
+_DB_ENGINE = config('DB_ENGINE', default='django.db.backends.mysql')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': _DB_ENGINE,
         'NAME': config('DB_NAME', default='trialguard_db'),
         'USER': config('DB_USER', default='root'),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'OPTIONS': (
+            {'sslmode': 'require'}
+            if 'postgresql' in _DB_ENGINE
+            else {'charset': 'utf8mb4', 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+        ),
     }
 }
 
